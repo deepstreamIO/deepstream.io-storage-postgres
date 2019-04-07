ebc75c6f2f68d595577e2ce6eba3fa3ddfeab5e9
Yasser Fadl <yasser.fadl@gmail.com>
2019-04-07 13:51:23 +0100

    ci: no longer push to s3

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,14 +3,14 @@
 echo "Before install - OS is $TRAVIS_OS_NAME"
 
 if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
-    echo "Installing postgres on mac"
-    echo "Updating homebrew"
-    brew update
-    echo "Installing and starting postgres"
-    brew install postgresql
-    rm -rf /usr/local/var/postgres
-    initdb /usr/local/var/postgres
+#    echo "Installing postgres on mac"
+#    echo "Updating homebrew"
+#    brew update
+#    echo "Installing and starting postgres"
+#    brew uninstall postgresql
+#    brew cleanup postgresql
+#    brew install postgresql@9.6
+#    rm -rf /usr/local/var/postgres
+#    initdb /usr/local/var/postgres
     pg_ctl -D /usr/local/var/postgres start
-    psql -c "CREATE DATABASE travis_ci_test;" -U postgres
-    psql -c "CREATE USER postgres WITH PASSWORD '';" -U postgres
 fi
