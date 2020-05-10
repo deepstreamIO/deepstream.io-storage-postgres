92aad34ad75b7bd8b5ede62097c92d86e4c15419
Yasser Fadl <yasser.fadl@gmail.com>
2020-05-10 13:18:54 +0800

    Fixing up postgres to use typescript, wip

---
+++ src/connector.ts
@@ -0,0 +1,294 @@
+import * as pg from 'pg'
+import * as pkg from '../package.json'
+import { DeepstreamPlugin, DeepstreamStorage, DeepstreamServices, StorageWriteCallback, StorageReadCallback, EVENT } from '@deepstream/types'
+import { DeepPartial, Dictionary } from 'ts-essentials'
+import { Statements} from './statements'
+import { SchemaListener, Noop, NotificationCallback } from './schema-listener'
+import { checkVersion, parseDSKey } from './utils'
+import { JSONObject } from '@deepstream/protobuf/dist/types/all'
+import { WriteOperation } from './write-operation'
+
+export interface KeyParameters {
+  schema: string,
+  table: string,
+  id: string,
+  owner?: string
+}
+
+export interface PostgresOptions {
+  useJsonb: any
+  splitChar: string,
+  table: {
+    defaultName: string
+  },
+  role: string,
+   user: string, // User to use for this connection
+       database: string, // Database to use. Every connection is scoped to a single db
+       password: string, // Password
+       host: string, // AWS HOST
+       port: number, // Optional postgres port
+       schema: string, // Optional schema
+       max: number, // Optional Maximum Parallel Connections
+       idleTimeoutMillis: number // Optional Connection will be cut after this time. Might disable it later for notification
+      writeInterval: number // Optional writes are buffered and flushed every [writeInterval] milliseconds
+     notifications: {
+        CREATE_TABLE: boolean,
+         DESTROY_TABLE: boolean,
+         INSERT: boolean,
+         UPDATE: boolean,
+         DELETE: boolean
+       }
+}
+
+const PostgresOptionsDefaults: DeepPartial<PostgresOptions> = {
+  splitChar: '/',
+  idleTimeoutMillis: 200,
+  writeInterval: 200,
+  table: {
+    defaultName: 'default'
+  },
+  notifications: {
+    CREATE_TABLE: true,
+    DESTROY_TABLE: true,
+    INSERT: true,
+    UPDATE: true,
+    DELETE: true
+  }
+}
+
+const UNDEFINED_TABLE = '42P01'
+const INTERNAL_ERROR = 'XX000'
+const DATABASE_IS_STARTING_UP = '57P03'
+const CONNECTION_REFUSED = 'ECONNREFUSED'
+
+/**
+ * Class deepstream.io postgres database connector
+ */
+export class Connector extends DeepstreamPlugin implements DeepstreamStorage {
+  public description: string
+  public options: PostgresOptions
+
+  private logger = { info: console.log, error: console.error }
+  private writeOperations: Dictionary<WriteOperation> = {}
+
+  private connectionPool!: pg.Pool
+  private schemaListener!: SchemaListener
+  private flushInterval!: NodeJS.Timer
+  constructor (options: DeepPartial<PostgresOptions>, private services: DeepstreamServices) {
+    super()
+    this.options = { ...PostgresOptionsDefaults, ...options } as PostgresOptions
+    this.description = `Postgres connection to ${this.options.host} and database ${this.options.database} ${pkg.version}`
+    this.statements = new Statements(this.options)
+  }
+
+  init () {
+    this.connectionPool = new pg.Pool(this.options)
+    this.connectionPool.on('error', this.checkError.bind(this))
+    this.schemaListener = new SchemaListener(this.connectionPool, this.logger)
+    this.flushInterval = setInterval(this.flushWrites.bind(this), this.options.writeInterval)
+  }
+
+  public async whenReady (): Promise<void> {
+    return new Promise((resolve, reject) => this.initialise((error) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve()
+      }
+    }))
+  }
+
+  public async close (): Promise<void> {
+    return new Promise((resolve) => this.destroy(resolve))
+  }
+
+  /**
+   * Destroys the connector. Closes the connection pool and
+   * all open listeners and stops the write loop
+   */
+  public destroy (callback: () => void) {
+    clearInterval(this.flushInterval)
+    this.schemaListener.destroy()
+    this.connectionPool.end(callback)
+  }
+
+  /**
+   * Creates a new schema.
+   */
+  public createSchema (name: string, callback: Noop) {
+    const statement = this.statements.createSchema({ name })
+    this.query(statement, callback, [], true)
+  }
+
+  /**
+   * Destroys a previously created schema and all the tables within it
+   */
+  public destroySchema (name: string, callback: Noop) {
+    const statement = this.statements.destroySchema({ name })
+    this.query( statement, callback, [], true )
+  }
+
+  /**
+   * Returns a list of all the tables within a given schema
+   * and the number of entries within each table
+   */
+  public getSchemaOverview (name: string, callback: (error: Error | null, tables?: Dictionary<number>) => void) {
+    name = name || this.options.schema
+    const statement = this.statements.getOverview({ schema: name })
+    this.query(statement, (error, result) => {
+      if (error || !result) {
+        callback(error)
+        return
+      }
+      const tables: Dictionary<number> = {}
+      for (let i = 0; i < result.rows.length; i++ ) {
+        tables[result.rows[i].table] = result.rows[ i ].entries
+      }
+      callback( null, tables )
+    }, [], true )
+  }
+
+  /**
+   * Subscribes to notifications for actions within a schema. Callback
+   * will be invoked every time a table was created or a record was created,
+   * updated or deleted
+   */
+  public subscribe (callback: NotificationCallback, done: Noop, schema?: string) {
+    schema = schema || this.options.schema
+    this.schemaListener.getNotificationsForSchema(schema, callback, done)
+  }
+
+  /**
+   * Remove a subscription that was previously established using getNotificationsForSchema
+   */
+  public unsubscribe (callback: NotificationCallback, done: Noop, schema: string) {
+    schema = schema || this.options.schema
+    this.schemaListener.unsubscribeFromNotificationsForSchema(schema, callback, done)
+  }
+
+  /**
+  * This will schedule a value to be written to the database. Writes are buffered and overwrite
+  * each other. At the end of this.options.writeInterval only the latest value will be written
+  */
+ public set (key: string, version: number, value: JSONObject, callback: StorageWriteCallback) {
+    const params = parseDSKey(key, this.options)
+    const tableName = params.schema + params.table
+
+    if (!this.writeOperations[tableName]) {
+      this.writeOperations[ tableName ] = new WriteOperation( params, this )
+    }
+
+    this.writeOperations[tableName].add(params.id, version, value, callback)
+  }
+
+  /**
+  * Retrieves a value from the database
+  */
+ public get (key: string, callback: StorageReadCallback) {
+    this.query<{ id: string, version: number, value: string }>(
+      this.statements.get(parseDSKey(key, this.options)),
+      (error: any, result) => {
+        if (error && error.code === UNDEFINED_TABLE) {
+          callback( null, -1, null )
+        }
+        else if (error || !result) {
+          callback( error )
+        }
+        else if (result.rows.length === 0 ) {
+          callback(null, -1, null)
+        }
+        else {
+          const { version, val } = result.rows[0]
+          callback(null, version, JSON.parse(val))
+        }
+    }, [], true )
+  }
+
+  /**
+  * Deletes a value from the database. If this was the last value for a given table
+  * it will also delete the table itself
+  */
+  public delete (key: string, callback: StorageWriteCallback) {
+    const statement = this.statements.delete(parseDSKey(key, this.options))
+    this.query(statement, (error) => callback(error ? error.toString() : null), [], false)
+  }
+
+  public deleteBulk (recordNames: string[], callback: StorageWriteCallback): void {
+    throw new Error('Method not implemented.')
+  }
+
+  /**
+   * Low level interface to execute postgreSQL queries.
+   */
+  public query<Result> (query: string, callback: (err: Error, result?: pg.QueryResult<any>) => void, args: any[] = [], silent: boolean = false) {
+    this.connectionPool.connect((error, client, done) => {
+      this.checkError(error, client)
+      if (error) {
+        callback(error)
+        return
+      }
+      client.query<Result>( query, args, (queryError, result) => {
+        done()
+        if (!silent) {
+          this.checkError(queryError, client)
+        }
+        callback(queryError, result)
+      })
+    })
+  }
+
+  /**
+   * Iterates through the buffered writeOperations every [writeInterval] milliseconds
+   * and either executes them if they have pending writes or clears them
+   * from the cache
+   */
+  private flushWrites () {
+    for (const tableName in this.writeOperations) {
+      if (this.writeOperations[tableName].isEmpty()) {
+        delete this.writeOperations[tableName]
+      } else {
+        this.writeOperations[tableName].execute()
+      }
+    }
+  }
+
+  /**
+   * Initialises the connector by creating a first connection
+   * to the db and execute a setup statement creating the initial
+   * global tables.
+   *
+   * As a final step this checks that the postgres version is >= 9.5
+   * which is the first version to support the ON CONFLICT statement
+   * for UPSERTS
+   */
+  public initialise (callback: Noop) {
+    this.query<any>(this.statements.initDb(this.options.schema), (error: any, result) => {
+      if (error) {
+        // retry for errors caused by concurrent initialisation
+        // or when the DB can't be reached (e.g. it's still starting up in a Docker setup)
+        if (error.code === INTERNAL_ERROR ||
+            error.code === DATABASE_IS_STARTING_UP ||
+            error.code === CONNECTION_REFUSED ) {
+          this.initialise(callback)
+          return
+        } else {
+          callback(error)
+          return
+        }
+      }
+
+      checkVersion((result as any)[4].rows[0].version)
+      callback(null)
+    }, [], true )
+  }
+
+  /**
+   * Basic check for errors. Just logs them to
+   * stdout
+   */
+  private checkError (error: any, client: pg.PoolClient) {
+    if ( error && error.code !== DATABASE_IS_STARTING_UP && error.code !== CONNECTION_REFUSED ) {
+      this.logger.info(EVENT.ERROR, error.namen)
+    }
+  }
+}
---
+++ src/schema-listener.ts
@@ -0,0 +1,115 @@
+import { EventEmitter } from 'events'
+import * as pg from 'pg'
+import { EVENT, NamespacedLogger } from '@deepstream/types'
+import { exists } from 'fs'
+
+export type NotificationCallback = ({ event, table, key }: { event: string, table: string, key: string}) => void
+export type Noop = (error: Error | null) => void
+
+/**
+ * This class subscribes to notification events for schemas
+ * triggered by NOTIFY statements.
+ *
+ * It creates events for CREATE_TABLE, DESTROY_TABLE, INSERT, UPDATE and DELETE
+ * each containing the table name and for INSERT, UPDATE and DELETE
+ * the key for the affected item
+ */
+export class SchemaListener {
+  private emitter = new EventEmitter()
+  private releaseConnection!: () => void
+  private client!: pg.PoolClient
+  /**
+   * Creates the SchemaListener. This doesn't trigger
+   * any database-interaction in itself
+   */
+  constructor (private connectionPool: pg.Pool, private logger: NamespacedLogger) {
+  }
+
+  /**
+   * Subscribes to notifications for a schema.
+   */
+  public getNotificationsForSchema (schema: string, callback: NotificationCallback, done: Noop) {
+    const isSubscribedToSchema = this.emitter.listenerCount(schema) > 0
+    this.emitter.on(schema, callback)
+    if (!isSubscribedToSchema) {
+      this.subscribeToSchema(schema, done)
+    }
+  }
+
+  /**
+   * Remove a subscription that was previously established using getNotificationsForSchema
+   */
+  public unsubscribeFromNotificationsForSchema (schema: string, callback: NotificationCallback | Noop, done: Noop): void {
+    if (arguments.length === 3) {
+      this.emitter.removeListener(schema, callback)
+    } else {
+      this.emitter.removeAllListeners(schema)
+    }
+
+    if (this.emitter.listenerCount(schema) === 0) {
+      this.client.query(`UNLISTEN ${schema};`, [], done)
+    }
+  }
+
+  /**
+   * Destroys the SchemaListener by releasing its persistent connection back
+   * into the pool
+   */
+  destroy () {
+    if (this.releaseConnection) {
+      this.releaseConnection()
+    }
+  }
+
+  /**
+   * Invoked for every notification received. Messages can have the
+   * following structure:
+   *
+   * CREATE_TABLE:<table-name>
+   * DESTROY_TABLE:<table-name>
+   * INSERT:<table-name>:<key>
+   * UPDATE:<table-name>:<key>
+   * DELETE:<table-name>:<key>
+   *
+   * This method will split combined notifications, e.g. for bulk upserts
+   * and emit them as individual events
+   */
+  private onNotification (msg: pg.Notification) {
+    const [event, table, ...keys] = msg.payload!.split(':')
+    if (keys.length === 0) {
+      this.emitter.emit(msg.channel, { event, table })
+    } else {
+      for (const key of keys) {
+        this.emitter.emit(msg.channel, { event, table, key })
+      }
+    }
+  }
+
+  /**
+   * Retrieves a connection from the pool and keeps it open until
+   * destroy is called
+   */
+  private connect (callback: () => void) {
+    this.connectionPool.connect((error, client, done) => {
+      if (error) {
+        this.logger.error(EVENT.ERROR, 'Error connecting to pg pool for schema listening')
+      }
+      this.client = client
+      this.releaseConnection = done
+      this.client.on('notification', this.onNotification.bind(this))
+      callback()
+    })
+  }
+
+  /**
+   * Subscribes for notifications to a specific topic/schema
+   */
+  private subscribeToSchema (schema: string, done: Noop) {
+    if (this.client) {
+      console.log('listening to', schema)
+      this.client.query(`LISTEN ${schema};`, done)
+    } else {
+      this.connect(this.subscribeToSchema.bind(this, schema, done))
+    }
+  }
+}
