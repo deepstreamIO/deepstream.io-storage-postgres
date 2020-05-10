b6810de7661506ec8fa421e3626a7c752fa2f538
Yasser Fadl <yasser.fadl@gmail.com>
2020-05-10 23:39:18 +0800

    test: use ci variables

--- test/cache-connector.spec.ts
+++ test/cache-connector.spec.ts
@@ -5,9 +5,9 @@ import { DeepstreamServices } from '@deepstream/types'
 
 const settings = {
   schema: 'test',
-  user: process.env.PGUSER || 'postgres',
-  database: process.env.PGDATABASE || 'deepstream',
-  password: process.env.PGPASSWORD || 'password',
+  user: process.env.POSTGRES_USER || 'postgres',
+  database: process.env.POSTGRES_DB || 'deepstream',
+  password: process.env.POSTGRES_PASSWORD || 'password',
   host: process.env.PGHOST || 'localhost',
   port: parseInt(process.env.PGPORT!, 10) || 5432,
   max: 10,
